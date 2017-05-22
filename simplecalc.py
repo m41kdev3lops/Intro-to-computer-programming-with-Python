298473da2d0ff511e54ace95663da7b07d357dfc
M41k <maikdevelops@gmail.com>
2017-05-22 03:50:45 +0200

    Simple Calc Added
    This is the final code for the simple calc program from episode 3.

---
+++ simplecalc.py
@@ -0,0 +1,9 @@
+print("Hello! welcome to SimpleCalc")
+print("Please enter the first number : ")
+first_number = input()
+print("Cool.. please enter the second number : ")
+second_number = input()
+
+total = int(first_number) + int(second_number)
+
+print(first_number + " + " + second_number + " = " + str(total))
