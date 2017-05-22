aac5efcb36bef71d81f0b8ccfb466e8cb0bbaf5a
M41k <maikdevelops@gmail.com>
2017-05-23 00:02:27 +0200

    Date Me.

---
+++ Episode 5/dateMe.py
@@ -0,0 +1,7 @@
+herName = "Madonna"
+herAge = 18
+
+if (herName.startswith('E') or herName.startswith('M')) and herAge >= 18 and herAge < 20:
+    print("Yeah! Let's fall in love baby")
+else:
+    print("Nah.. u don't fit me")
